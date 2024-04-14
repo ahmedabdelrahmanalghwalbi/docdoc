4557f627e5726f5487a96cf9c5f21a8abe846696
ahmedabdelrahmanalghwalbi <ahmedabdelrahman4999@gmail.com>
2024-04-14 21:09:48 +0200

    doc doc authentication

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:docdoc/core/networking/api_constants.dart';
+import 'package:docdoc/features/login/data/models/login_request_body.dart';
+import 'package:docdoc/features/login/data/models/login_response.dart';
+import 'package:retrofit/retrofit.dart';
+import '../../features/sign_up/data/models/sign_up_request_body.dart';
+import '../../features/sign_up/data/models/sign_up_response.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.login)
+  Future<LoginResponse> login(
+    @Body() LoginRequestBody loginRequestBody,
+  );
+
+  @POST(ApiConstants.signup)
+  Future<SignupResponse> signup(
+    @Body() SignupRequestBody signupRequestBody,
+  );
+}
---
+++ lib/features/login/ui/widgets/dont_have_account_text.dart
@@ -0,0 +1,32 @@
+import 'package:docdoc/core/helpers/extensions.dart';
+import 'package:docdoc/core/routing/routes.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import '../../../../core/theming/styles.dart';
+
+class DontHaveAccountText extends StatelessWidget {
+  const DontHaveAccountText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'Don\'t have an account?',
+            style: TextStyles.font13DarkBlueRegular,
+          ),
+          TextSpan(
+            text: ' Sign Up',
+            style: TextStyles.font13BlueSemiBold,
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                context.pushReplacementNamed(Routes.signUpScreen);
+              },
+          ),
+        ],
+      ),
+    );
+  }
+}
